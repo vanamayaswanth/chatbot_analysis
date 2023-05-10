e1434650ab79707a7ca6998e44da535e9ebc994a
sethusaim <sethusaim@gmail.com>
2023-05-10 18:24:50 +0530

    added code

---
+++ demo.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import time
+
+from main import classify
+
+# ## Step 1 - Calculate tokens for Conversations
+
+# ## Output will be test.csv file containing NumTokens for each conversation
+
+# df = pd.read_excel("Chat Statistics.xlsx",header=2)
+
+# generate_num_tokens(dataframe=df["Conversations"])
+
+## Step 2 - Read the test.csv, and filter the tokens which are less than 4000
+
+## Output will be tokenized_data.csv file containing all the conversations less than 4000 tokens
+
+# df = pd.read_csv("test.csv")
+
+# filtered_df = df[df['NumTokens'] < 4000].dropna()
+
+# df.to_csv("tokenized_data.csv",index=False,header=True)
+
+## Step 3 - Call the classify_query function and execute the code
+
+## Output will final_responses.csv containing tech or non tech queries
+
+start_time = time.time()
+
+df = pd.read_csv("tokenized_data.csv")
+
+_df = df["Conversations"]
+
+new_df = pd.DataFrame(columns=["Conversations","Classifaction"])
+
+lst = [classify(str(i)) for i in _df]
+
+new_df["Conversations"] = _df
+
+new_df["Classifaction"] = lst
+
+new_df.to_csv("final-100.csv", index=False, header=True)
+
+end_time = time.time()
+
+elapsed_time = (end_time - start_time) / 3600
+
+print("Elapsed Time:", elapsed_time, "hours")
+
--- main.py
+++ main.py
@@ -1,16 +1,57 @@
-str1 = "Haveing beautful place place something the the this it the it it it"
+import time
 
-lst = str1.split()
+import openai
+import pandas as pd
+import tiktoken
+from openai.error import InvalidRequestError
+from tenacity import retry, stop_after_attempt, wait_random_exponential
 
-dic = {}
 
-for i in lst:
-    if i in dic:
-        dic[i] += 1
-        
-    else:
-        dic[i] = 1
-        
-Keymax = max(zip(dic.values(), dic.keys()))[1]
+def num_tokens_from_string(string: str) -> int:
+    try:
+        encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
 
-print(len(Keymax))
+        num_tokens = len(encoding.encode(string))
+
+        return num_tokens
+
+    except Exception as e:
+        raise e
+
+
+@retry(wait=wait_random_exponential(min=1, max=60), stop=stop_after_attempt(6))
+def classify(chat: str) -> str:
+    try:
+        res = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            temperature=0.5,
+            messages=[
+                {
+                    "role": "system",
+                    "content": "You are chat analyst, perform conversation classifaction into one of the following categories: tech, non-tech. If the converstation consists of both tech and non tech return mixed as the answer .Return the output in one word",
+                },
+                {"role": "user", "content": f"{str(chat)}"},
+            ],
+        )
+
+        print(f'Response {res["choices"][0]["message"]["content"]}')
+
+        return res["choices"][0]["message"]["content"]
+
+    except InvalidRequestError:
+        print("Token exceeded")
+
+    except Exception as e:
+        raise e
+
+
+def generate_num_tokens(dataframe: pd.DataFrame):
+    try:
+        dataframe["NumTokens"] = [
+            num_tokens_from_string(str(i)) for i in dataframe["Conversations"]
+        ]
+
+        dataframe.to_csv("test.csv", index=False, header=True)
+
+    except Exception as e:
+        raise e
